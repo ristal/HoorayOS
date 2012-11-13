1a79399851c81c5b324570cf56ed3d7514787de3
胡尐睿丶 <304327508@qq.com>
2012-11-14 00:17:21 +0800

    优化翻页导航代码

--- js/hros.navbar.js
+++ js/hros.navbar.js
@@ -14,7 +14,6 @@ HROS.navbar = (function(){
 			HROS.navbar.getAvatar();
 			HROS.navbar.setAvatar();
 			HROS.navbar.move();
-			HROS.navbar.deskSwitch();
 		},
 		/*
 		**  获取头像
@@ -47,35 +46,7 @@ HROS.navbar = (function(){
 		**  拖动
 		*/
 		move : function(){
-			$('#nav-bar').on('mousedown', function(e){
-				if(e.button == 0 || e.button == 1){
-					var x, y, cx, cy, lay, obj = $('#nav-bar');
-					x = e.clientX - obj.offset().left;
-					y = e.clientY - obj.offset().top;
-					//绑定鼠标移动事件
-					$(document).on('mousemove', function(e){
-						lay = HROS.maskBox.desk();
-						lay.show();
-						cx = e.clientX - x <= 0 ? 0 : e.clientX - x > $(document).width() - 210 ? $(document).width() - 210 : e.clientX - x;
-						cy = e.clientY - y <= 10 ? 10 : e.clientY - y > $(document).height() - 50 ? $(document).height() - 50 : e.clientY - y;
-						obj.css({
-							left : cx,
-							top : cy
-						});
-					}).on('mouseup', function(){
-						if(typeof(lay) !== 'undefined'){
-							lay.hide();
-						}
-						$(this).off('mousemove').off('mouseup');
-					});
-				}
-			});
-		},
-		/*
-		**  点击切换
-		*/
-		deskSwitch : function(){
-			$('#nav-bar .nav-container').on('mousedown', 'a.indicator', function(e){
+			$('#nav-bar, #nav-bar .nav-container a.indicator').on('mousedown', function(e){
 				$('.popup-menu').hide();
 				$('.quick_view_container').remove();
 				if(e.button == 0 || e.button == 1){
@@ -97,25 +68,8 @@ HROS.navbar = (function(){
 					}).on('mouseup', function(){
 						if(dx == cx && dy == cy){
 							if(typeof(thisobj.attr('index')) !== 'undefined'){
-								var nav = $('#navContainer'), currindex = HROS.CONFIG.desk, switchindex = thisobj.attr('index'),
-								currleft = $('#desk-' + currindex).offset().left, switchleft = $('#desk-' + switchindex).offset().left;
-								if(currindex != switchindex){
-									if(!$('#desk-' + switchindex).hasClass('animated') && !$('#desk-' + currindex).hasClass('animated')){
-										$('#desk-' + currindex).addClass('animated').animate({
-											left : switchleft
-										}, 500, 'easeInOutCirc', function(){
-											$(this).removeClass('animated');
-										});
-										$('#desk-'+switchindex).addClass('animated').animate({
-											left : currleft
-										}, 500, 'easeInOutCirc', function(){
-											$(this).removeClass('animated');
-											nav.removeClass('nav-current-' + currindex).addClass('nav-current-' + switchindex);
-											HROS.CONFIG.desk = switchindex;
-										});
-									}
-								}
-							}else{
+								HROS.navbar.switchDesk(thisobj.attr('index'));
+							}else if(thisobj.hasClass('indicator-manage')){
 								//初始化全局视图
 								HROS.appmanage.init();
 							}
@@ -127,6 +81,32 @@ HROS.navbar = (function(){
 					});
 				}
 			});
+		},
+		/*
+		**  切换桌面
+		*/
+		switchDesk : function(deskNumber){
+			//验证传入的桌面号是否为1-5的正整数
+			var r = /^\+?[1-5]*$/;
+			deskNumber = r.test(deskNumber) ? deskNumber : 1;
+			var nav = $('#navContainer'), currindex = HROS.CONFIG.desk, switchindex = deskNumber,
+			currleft = $('#desk-' + currindex).offset().left, switchleft = $('#desk-' + switchindex).offset().left;
+			if(currindex != switchindex){
+				if(!$('#desk-' + switchindex).hasClass('animated') && !$('#desk-' + currindex).hasClass('animated')){
+					$('#desk-' + currindex).addClass('animated').animate({
+						left : switchleft
+					}, 500, 'easeInOutCirc', function(){
+						$(this).removeClass('animated');
+					});
+					$('#desk-'+switchindex).addClass('animated').animate({
+						left : currleft
+					}, 500, 'easeInOutCirc', function(){
+						$(this).removeClass('animated');
+						nav.removeClass('nav-current-' + currindex).addClass('nav-current-' + switchindex);
+						HROS.CONFIG.desk = switchindex;
+					});
+				}
+			}
 		}
 	}
 })();
